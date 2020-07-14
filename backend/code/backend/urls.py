04fdb9db77c75a0634805f5c337a778c03ab478c
Ivan Buccella <ivan@buccella.me>
2020-07-14 19:24:13 +0200

    Created two api endpoints on backend: one for user token authentication, one for testing the authentication.

--- backend/code/backend/urls.py
+++ backend/code/backend/urls.py
@@ -15,12 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from rest_framework import routers
 from backend import views
 
-router = routers.DefaultRouter()
-
 urlpatterns = [
-    path('', include(router.urls)),
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("hello", views.HelloView.as_view(), name="hello"),
+    path("auth-user", views.AuthUser.as_view(), name="auth-user"),
 ]
--- backend/code/backend/views.py
+++ backend/code/backend/views.py
@@ -1,2 +1,36 @@
 from django.contrib.auth.models import User, Group
-from rest_framework import viewsets
+from rest_framework import parsers
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
+
+
+class HelloView(APIView):
+    """
+    API endpoint that return received Token only for authenticated users.
+    """
+
+    parser_classes = (parsers.JSONParser,)
+
+    def post(self, request, *args, **kwargs):
+        token = request.data["token"]
+        content = {"message": "Your token is: " + token}
+        return Response(content)
+
+
+class AuthUser(ObtainAuthToken):
+    """
+    API endpoint that return token for user authentication.
+    """
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(
+            data=request.data, context={"request": request}
+        )
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data["user"]
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({"token": token.key,})
+
