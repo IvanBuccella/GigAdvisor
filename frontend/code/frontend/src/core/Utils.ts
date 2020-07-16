fd30c033197744b4862718434221bb20dd409da8
Ivan Buccella <ivan@buccella.me>
2020-07-16 23:39:43 +0200

    Updated Utils class.

--- frontend/code/frontend/src/core/Utils.ts
+++ frontend/code/frontend/src/core/Utils.ts
@@ -7,16 +7,45 @@ export class Utils {
     return config.API_ENDPOINT;
   }
 
-  async postCall(url: string, data: string, token: string) {
+  getUserToken() {
+    let auth: any;
+    auth = localStorage.getItem("ga-auth");
+    if (auth != undefined) {
+      let token = JSON.parse(auth).token;
+      if (token != undefined) {
+        return token;
+      }
+    }
+    return null;
+  }
+
+  isAuthenticatedUser() {
+    let token = this.getUserToken();
+    if (token != null) {
+      return true;
+    }
+    return false;
+  }
+
+  async postCall(url: string, data: string) {
+    let token = this.getUserToken();
+    let headers = {};
+    if (token != null) {
+      headers = {
+        "Content-Type": "application/json",
+        Authorization: "Token " + token,
+      };
+    } else {
+      headers = {
+        "Content-Type": "application/json",
+      };
+    }
     const response = await fetch(url, {
       method: "POST", // *GET, POST, PUT, DELETE, etc.
       mode: "cors", // no-cors, *cors, same-origin
       cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
       credentials: "same-origin", // include, *same-origin, omit
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: token,
-      },
+      headers: headers,
       redirect: "follow", // manual, *follow, error
       referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
       body: data, // body data type must match "Content-Type" header
