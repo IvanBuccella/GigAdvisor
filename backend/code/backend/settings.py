5216bd087b7066c65dc035baae5bb69ece8f69f9
Ivan Buccella <ivan@buccella.me>
2020-10-07 15:41:17 +0200

    Added static files configurations on backend.

--- backend/code/backend/settings.py
+++ backend/code/backend/settings.py
@@ -129,9 +129,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "backend", "site_static")]
+STATIC_ROOT = os.path.join(BASE_DIR, "backend", "static")
+MEDIA_ROOT = os.path.join(BASE_DIR, "backend", "media")
 STATIC_URL = "/static/"
-
+MEDIA_URL = "/media/"
 
 REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
