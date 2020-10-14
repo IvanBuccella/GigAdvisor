160f3c5c6e95542a3f4848f42a941ab24a33c9de
Ivan Buccella <ivan@buccella.me>
2020-10-14 23:42:44 +0200

    Code refactoring on frontend.

--- frontend/code/frontend/src/pages/Signup.tsx
+++ frontend/code/frontend/src/pages/Signup.tsx
@@ -43,7 +43,7 @@ const Signup: React.FC = () => {
           setSignupOk(true);
           setTimeout(function () {
             utilities.pageRedirect("login");
-          }, 5000);
+          }, 2000);
         } else {
           setSignupError(true);
         }
